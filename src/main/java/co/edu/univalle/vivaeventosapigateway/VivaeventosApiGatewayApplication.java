c3938c50c12623eeaca39f29f526eff936c1410c
Vane2005 <vanessaduranmona@gmail.com>
2026-04-17 17:47:31 -0500

    Envia estructura base del proyecto

---
+++ src/main/java/co/edu/univalle/vivaeventosapigateway/VivaeventosApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package co.edu.univalle.vivaeventosapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivaeventosApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VivaeventosApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/univalle/vivaeventosapigateway/VivaeventosApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.univalle.vivaeventosapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivaeventosApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
